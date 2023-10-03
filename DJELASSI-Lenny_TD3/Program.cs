e95cf43789d0f3b589e83c89e8ab494c4558862c
Lenny Djelassi <lennydjelassi@outlook.fr>
2023-10-03 11:01:01 +0200

    Update Program.cs

--- DJELASSI-Lenny_TD3/Program.cs
+++ DJELASSI-Lenny_TD3/Program.cs
@@ -22,13 +22,13 @@ namespace TD3
                 switch (choix)
                 {
                     case 1: //si le choix est 1 alors 
-                        ReadFile("Etudiants.txt");
+                        ReadFile("Etudiants.txt"); //Appel de la fonction ReadFile
                         break;
                     case 2: //si le choix est 2 alors
-                        ReadFile2("Etudiants.txt");
+                        ReadFile2("Etudiants.txt"); //Appel de la fonction ReadFile2
                         break;
                     case 3: //si le choix est 3 alors
-                        ReadFile3("Etudiants.txt");
+                        ReadFile3("Etudiants.txt"); //Appel de la fonction ReadFile3
                         break;
                     default:
                         Console.WriteLine("Choix invalide. Veuillez choisir entre 1, 2 ou 3.");
@@ -46,46 +46,47 @@ namespace TD3
         //Première fonction 
         private static void ReadFile(string fileName)
         {
-            StreamReader lecteur = new StreamReader(fileName);
-            String line = "";
-            int numberOfChar = 0;
+            StreamReader lecteur = new StreamReader(fileName); //Instanciation 
+            String line = ""; //Initialisation de line une chaine de caractères vide
+            int numberOfChar = 0; //Initialisation de la variable numberOfChar un entier égal à 0
 
-            while (line != null)
+            while (line != null) //tant que la ligne lu n'est pas nulle
             {
-                line = lecteur.ReadLine();
-                if (line != null)
+                line = lecteur.ReadLine(); //lis la ligne
+                if (line != null) //si la ligne n'est pas nulle
                 {
-                    numberOfChar += line.Length;
-                    Console.WriteLine(line);
+                    numberOfChar += line.Length; //Calcul le nombre de caractères dans la ligne
+                    Console.WriteLine(line); //Affiche la ligne
                 }
             }
             lecteur.Close();
         }
-
+        
+        //Deuxième fonction
         private static void ReadFile2(string fileName)
         {
-            StreamReader lecteur = new StreamReader(fileName);
-            string line;
-            int lineNumber = 0;
-            bool evenLine = false;
+            StreamReader lecteur = new StreamReader(fileName); //Instanciation de lecteur un nouvel objet
+            string line; //Déclaration de line une chaine de caractère
+            int lineNumber = 0; //Initialisation de lineNumber en entier égal à 0
+            bool evenLine = false; //Initialisation de evenLine un booléen égal à false
 
-            while ((line = lecteur.ReadLine()) != null)
+            while ((line = lecteur.ReadLine()) != null) //tant que les lignes lues ne sont pas égal à nulle
             {
-                lineNumber++;
-                if (lineNumber % 2 == 0)
+                lineNumber++; //ajoute 1 à lineNumber
+                if (lineNumber % 2 == 0) //Si la ligne est paire
                 {
                     // Ligne paire, utilisez une couleur différente ici
                     Console.ForegroundColor = ConsoleColor.Green;
                 }
-                else
+                else //sinon elle est impaire
                 {
                     // Ligne impaire, utilisez une autre couleur ici
                     Console.ForegroundColor = ConsoleColor.Blue;
                 }
 
-                string[] parts = line.Split('\t');
+                string[] parts = line.Split('\t'); //initalisation de parts un tableau de chaine de caractères qui met chaque partie séparée par une tabulation dans une colonne du tableau
 
-                if (parts.Length >= 3)
+                if (parts.Length >= 3) //si la longueur de parts est supérieure ou égale à 3 alors
                 {
                     string nom = parts[0].Trim();
                     string prenom = parts[1].Trim().ToUpper(); // Met le prénom en majuscule
@@ -119,30 +120,31 @@ namespace TD3
             lecteur.Close();
         }
 
+        //Troisième fonction
         private static void ReadFile3(string fileName)
         {
-            StreamReader lecteur = new StreamReader(fileName);
-            string line;
-            int total = 0;
-            int hommes = 0;
-            int femmes = 0;
+            StreamReader lecteur = new StreamReader(fileName); //Instanciation de lecteur un nouvel objet
+            string line; //Déclaration de line une chaine de caractère
+            int total = 0; //Initialisation de total un entier égal à 0
+            int hommes = 0; //Initialisation de hommes un entier égal à 0
+            int femmes = 0; //Initialisation de femmes un entier égal à 0
 
-            while ((line = lecteur.ReadLine()) != null)
+            while ((line = lecteur.ReadLine()) != null) //tant que les lignes lues ne sont pas égal à nulle
             {
-                string[] parts = line.Split('\t');
+                string[] parts = line.Split('\t'); //initalisation de parts un tableau de chaine de caractères qui met chaque partie séparée par une tabulation dans une colonne du tableau
 
-                if (parts.Length >= 4)
+                if (parts.Length >= 4) //si la longueur de parts est supérieure ou égale à 4 alors
                 {
-                    string sexe = parts[3].Trim();
-                    total++;
+                    string sexe = parts[3].Trim(); //On va directement dans la colonne qui correspond au sexe de l'étudiant
+                    total++; //On ajoute 1 au total
 
-                    if (sexe.Equals("G", StringComparison.OrdinalIgnoreCase))
+                    if (sexe.Equals("G", StringComparison.OrdinalIgnoreCase)) //Si le sexe est G
                     {
-                        hommes++;
+                        hommes++; //Ajoute 1 à l'entier hommes
                     }
-                    else if (sexe.Equals("F", StringComparison.OrdinalIgnoreCase))
+                    else if (sexe.Equals("F", StringComparison.OrdinalIgnoreCase)) //Si le sexe est F
                     {
-                        femmes++;
+                        femmes++; //Ajoute 1 à l'entier femmes
                     }
                 }
             }
